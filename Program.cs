8b638cb856e037bbbae2b1eb6255fb0e36280144
Rodolfo Rodarte Jr <rodartejr.rodolfo@hotmail.com>
2020-10-21 21:38:04 -0500

    Primer Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App02_CalcularSueldo_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horasTrabajadas;
+            double costoHora, sueldo;
+            string linea;
+
+            Console.Write("Ingrese horas trabajadas por el operario: ");
+            linea = Console.ReadLine();
+            horasTrabajadas = int.Parse(linea);
+
+            Console.Write("Ingrese el pago por hora: ");
+            linea = Console.ReadLine();
+            costoHora = double.Parse(linea);
+
+            sueldo = costoHora * horasTrabajadas;
+            Console.Write("\nEl Sueldo es: "+sueldo);
+            Console.ReadKey();
+            
+        }
+    }
+}
